e14411042368ca5d48997d3e6710d3681c128d01
Nergon <enderman3025@gmail.com>
2024-12-29 14:24:10 +0000

    TFT config update

--- src/EXT/TFT_Config.h
+++ src/EXT/TFT_Config.h
@@ -2,60 +2,27 @@
 *FROM NERGON:
 *THIS CONFIG IS FROM TFT_eSPI LIBRARY BY Bodmer
 */
+#undef USER_SETUP_INFO
+#define USER_SETUP_INFO "TFT_CONFIG"
 
-
-
-
-
-
-
-
-
-
-
-
-
-#define USER_SETUP_INFO "User_Setup"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#ifndef ILI9341_DRIVER
 #define ILI9341_DRIVER       
+#endif
+
+#undef TFT_MISO 
+#undef TFT_MOSI 
+#undef TFT_SCLK 
+#undef TFT_CS     
+#undef TFT_DC     
+#undef TFT_RST    
+#undef TFT_BL     
 
 #define TFT_MISO 12
 #define TFT_MOSI 23
 #define TFT_SCLK 18
 #define TFT_CS   27  
 #define TFT_DC   32  
-#define TFT_RST   4  
+#define TFT_RST  -1  
 #define TFT_BL   04  
 
 
